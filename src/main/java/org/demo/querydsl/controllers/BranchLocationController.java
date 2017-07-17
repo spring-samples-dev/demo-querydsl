0e4b9a0d51886aae08e824bfe6c3ca2a3f8f9e05
Adnan Mamajiwala <rawkz.star@gmail.com>
2017-07-17 06:03:11 -0400

    Finishes post and get methods of the controller with querydsl.

--- src/main/java/org/demo/querydsl/controllers/BranchLocationController.java
+++ src/main/java/org/demo/querydsl/controllers/BranchLocationController.java
@@ -1,10 +1,11 @@
 package org.demo.querydsl.controllers;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Predicate;
 import lombok.RequiredArgsConstructor;
 import org.demo.querydsl.entities.BranchLocation;
+import org.demo.querydsl.entities.QBranchLocation;
 import org.demo.querydsl.repositories.BranchLocationRepository;
-import org.demo.querydsl.repositories.MyUserRepository;
 import org.springframework.data.querydsl.binding.QuerydslPredicate;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,21 +15,26 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/branch")
 public class BranchLocationController {
 
-    private final MyUserRepository repository;
-    private final BranchLocationRepository branchLocationRepository;
+    private final BranchLocationRepository repository;
 
     @PostMapping(path = "/find")
-    public Iterable<BranchLocation> find(@RequestBody @QuerydslPredicate(root = BranchLocation.class) Predicate predicate){
-        System.out.println(predicate.toString());
-//        Example<BranchLocation> example = new Example<BranchLocation>();
-//        return repository.findAll(example);
+    public Iterable<BranchLocation> find(@RequestBody BranchLocation branchLocation) {
+        System.out.println(branchLocation.toString());
+        QBranchLocation qb = QBranchLocation.branchLocation;
+        BooleanBuilder builder = new BooleanBuilder();
+        if (branchLocation.getState() != null) {
+            builder.and(qb.state.equalsIgnoreCase(branchLocation.getState()));
+        }
+        if (branchLocation.getCity() != null) {
+            builder.and(qb.city.equalsIgnoreCase(branchLocation.getCity()));
+        }
 
-        return repository.findAll(predicate);
+        return repository.findAll(builder);
     }
 
 
     @GetMapping(value = "/search")
-    public Iterable<BranchLocation> findAllByWebQuerydsl(@QuerydslPredicate(root = BranchLocation.class) Predicate predicate) {
+    public Iterable<BranchLocation> search(@QuerydslPredicate(root = BranchLocation.class) Predicate predicate) {
         System.out.println(predicate.toString());
         return repository.findAll(predicate);
     }
--- src/main/java/org/demo/querydsl/repositories/BranchLocationRepository.java
+++ src/main/java/org/demo/querydsl/repositories/BranchLocationRepository.java
@@ -1,8 +1,23 @@
 package org.demo.querydsl.repositories;
 
+import com.querydsl.core.types.dsl.StringExpression;
+import com.querydsl.core.types.dsl.StringPath;
 import org.demo.querydsl.entities.BranchLocation;
+import org.demo.querydsl.entities.QBranchLocation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.querydsl.binding.SingleValueBinding;
 
-public interface BranchLocationRepository  extends JpaRepository<BranchLocation, Long>, QueryDslPredicateExecutor<BranchLocation> {
+public interface BranchLocationRepository  extends JpaRepository<BranchLocation, Long>,
+        QueryDslPredicateExecutor<BranchLocation> ,
+        QuerydslBinderCustomizer<QBranchLocation> {
+
+
+    @Override
+    default void customize(QuerydslBindings bindings, QBranchLocation root) {
+        bindings.bind(String.class).
+                first((SingleValueBinding<StringPath, String>) StringExpression::containsIgnoreCase);
+    }
 }
